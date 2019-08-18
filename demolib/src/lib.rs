df245d54be24b50e22807a731336f960e1e94854
unknown <48052798@qq.com>
2019-08-18 20:40:04 +0800

    add fly account demo

--- demolib/src/lib.rs
+++ demolib/src/lib.rs
@@ -1,15 +1,3 @@
-#[cfg(test)]
-
-use flycoin;
-
-mod demo_mod {
-    mod waiter {
-        fn add_order(){ print!("add_order")}
-        fn set_table() {print!("set_table")}
-    }
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
+mod fly_result;
+mod fly_coin;
+mod fly_account;
