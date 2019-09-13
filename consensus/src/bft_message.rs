8f47bae5cd32fcb78b8004914ef8aed693dec91d
unknown <48052798@qq.com>
2019-09-13 10:49:30 +0800

    add consensue method code

--- consensus/src/bft_message.rs
+++ consensus/src/bft_message.rs
@@ -30,6 +30,10 @@ impl Bft_Message {
         let digest = Bft_Message_Bytes(source_msg_bytes).hash();
         return digest;
     }
+
+    pub fn get_id(&self) -> &str {
+        return self.id.as_str()
+    }
 }
 
 ///  use to contain the byte[] of Bft_Message
@@ -51,20 +55,20 @@ impl<'a> CryptoHash for Bft_Message_Bytes<'a> {
 ///  every Bft node non fault will send the repay;
 #[derive(Serialize, Deserialize)]
 pub struct Bft_Replay{
-    view_num:u32,
+    view_num:u64,
     timestamp:u64,
     client_id:String,
-    node_id:String,
+    node_id:u64,
     payload:String
 }
 
 impl Bft_Replay {
-    pub fn new(_view_num:u32, _payload: &str, _node_id:&str, _source_msg:Bft_Message)->Bft_Replay {
+    pub fn new(_view_num:u64, _payload: &str, _node_id:u64, _source_msg:Bft_Message)->Bft_Replay {
         let replay = Bft_Replay {
             view_num:_view_num,
             timestamp:_source_msg.timestamp,
             client_id:_source_msg.client_id,
-            node_id:_node_id.to_string(),
+            node_id:_node_id,
             payload:_payload.to_string()
         };
         return replay;
@@ -72,14 +76,14 @@ impl Bft_Replay {
 }
 #[derive(Serialize, Deserialize)]
 pub struct Bft_PrePrepare_Message {
-    view_num:u32,
-    sequence_num:u32,
+    view_num:u64,
+    sequence_num:u64,
     msg_digest:HashValue,
     client_msg:Bft_Message,
 }
 
 impl Bft_PrePrepare_Message {
-    pub fn new(_view_num:u32, _sequence_num: u32,_source_msg:Bft_Message)->Bft_PrePrepare_Message {
+    pub fn new(_view_num:u64, _sequence_num: u64,_source_msg:Bft_Message)->Bft_PrePrepare_Message {
 
 
         let replay = Bft_prepare_Message {
@@ -91,11 +95,11 @@ impl Bft_PrePrepare_Message {
         return replay;
     }
 
-    pub fn get_view_num(&self) -> u32 {
+    pub fn get_view_num(&self) -> u64 {
         return self.view_num.clone();
     }
 
-    pub fn get_sequence_num(&self) -> u32 {
+    pub fn get_sequence_num(&self) -> u64 {
         return self.sequence_num.clone();
     }
 
@@ -110,28 +114,28 @@ impl Bft_PrePrepare_Message {
 }
 #[derive(Serialize, Deserialize)]
 pub struct Bft_Prepare_Message {
-    view_num:u32,
-    sequence_num:u32,
-    msg_digest:HashValue,
-    node_id:u32
+    view_num:u64,
+    sequence_num:u64,
+    msg_digest:String,
+    node_id:u64
 }
 #[derive(Serialize, Deserialize)]
 impl Bft_Prepare_Message {
-    pub fn new(_view_num:u32, _sequence_num:u32, _digest:HashValue, _node_id:u32 ) ->Bft_Prepare_Message {
+    pub fn new(_view_num:u64, _sequence_num:u64, _digest:&str, _node_id:u64 ) ->Bft_Prepare_Message {
         let msg = Bft_Prepare_Message {
             view_num:_view_num,
             sequence_num:_sequence_num,
-            msg_digest:_digest,
+            msg_digest:_digest.to_string(),
             node_id:_node_id
         };
         return msg;
     }
 
-    pub fn get_view_num(&self) -> u32 {
+    pub fn get_view_num(&self) -> u64 {
         return self.view_num.clone();
     }
 
-    pub fn get_sequence_num(&self) -> u32 {
+    pub fn get_sequence_num(&self) -> u64 {
         return self.sequence_num.clone();
     }
 
@@ -139,33 +143,37 @@ impl Bft_Prepare_Message {
         return &(self.client_msg);
     }
 
-    pub fn get_msg_digest(&self) -> &HashValue {
+    pub fn get_msg_digest(&self) -> &String {
         return &(self.msg_digest);
+    }
+
+    pub fn get_node_id(&self) -> u64 {
+        return self.node_id.clone();
     }
 }
 #[derive(Serialize, Deserialize)]
 pub struct Bft_Commit_Message {
-    view_num:u32,
-    sequence_num:u32,
-    msg_digest:HashValue,
-    node_id:u32
+    view_num:u64,
+    sequence_num:u64,
+    msg_digest:String,
+    node_id:u64
 }
 #[derive(Serialize, Deserialize)]
 impl Bft_Commit_Message {
-    pub fn new(_view_num:u32, _sequence_num:u32, _digest:HashValue, _node_id:u32 ) ->Bft_Commit_Message {
+    pub fn new(_view_num:u64, _sequence_num:u64, _digest:&str, _node_id:u64 ) ->Bft_Commit_Message {
         let msg = Bft_Commit_Message {
             view_num:_view_num,
             sequence_num:_sequence_num,
-            msg_digest:_digest,
+            msg_digest:_digest.to_string(),
             node_id:_node_id
         };
         return msg;
     }
-    pub fn get_view_num(&self) -> u32 {
+    pub fn get_view_num(&self) -> u64 {
         return self.view_num.clone();
     }
 
-    pub fn get_sequence_num(&self) -> u32 {
+    pub fn get_sequence_num(&self) -> u64 {
         return self.sequence_num.clone();
     }
 
--- consensus/src/bft_node.rs
+++ consensus/src/bft_node.rs
@@ -7,14 +7,15 @@ extern crate serde_json;
 use serde_json;
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
+use std::collections::HashMap;
 use reqwest;
 
 #[derive(Serialize, Deserialize)]
 pub struct Btf_Node_Simple {
     node_id:u64,
-    address:String,
-    port:String,
-    public_key: String,
+    pub address:String,
+    pub port:String,
+    pub public_key: String,
 }
 
 pub struct Btf_Node{
@@ -22,9 +23,9 @@ pub struct Btf_Node{
     status:String,
     view_num:u64,
     node_list:Vec<Btf_Node_Simple>,
-    msg_cache:Vec<(u64, Bft_Message)>,
-    prepare_cache: Vec<(u64, Vec<Bft_Prepare_Message>)>,
-    commit_cache: Vec<(u64, Vec<Bft_Commit_Message>)>,
+    msg_cache:HashMap<u64, Bft_Message>,
+    prepare_cache: HashMap<u64, Vec<Bft_Prepare_Message>>,
+    commit_cache: HashMap<u64, Vec<Bft_Commit_Message>>,
     private_key: String,
 }
 
@@ -45,56 +46,243 @@ impl Btf_Node {
             msg_cache:Vec::new(),
             prepare_cache:Vec::new(),
             commit_cache:Vec::new(),
-            private_key: Ed25519PrivateKey,
+            private_key: "private".to_string(),
         };
 
         return node;
     }
 
-    fn doPrepare(&self, msg:Bft_PrePrepare_Message) {
-        // check is the primary message, check the digest by primary pub key;
-
-        // do digest by this node
-        let digest;
-        // new the prepare msg for this node
-
-        let prepaireMsg = Bft_Prepare_Message::new(self.view_num, msg.get_sequence_num(), digest, self.node_id );
-
-        // broadcast the msg to other
+    pub fn get_node_base(&self) -> &Btf_Node_Simple {
+        return &(self.base);
     }
 
-    fn doCommit(&self, _sequence_num:u64) {
+    pub fn doPrepare(& mut self, msg:Bft_PrePrepare_Message) {
+        // check is the primary message, check the digest by primary pub key;
+        if msg.get_view_num() != self.view_num {
+            return;
+        }
+
+        // check if have before,if not put msg to msg_cache
+        let mut source_msg_option:Option<Bft_Message> = Option::None;
+        if self.msg_cache.contains_key(& msg.get_sequence_num()) {
+            // have receive this msg num before, check if the same msg
+            let receive_msg = self.msg_cache.get(& msg.get_sequence_num()).unwrap();
+            if receive_msg.get_id() == msg.get_client_msg().get_id(){
+                // the same
+                source_msg_option = Some(receive_msg.clone());
+
+                // find the pre cache
+                for prepare_msg in self.prepare_cache.get(& msg.get_sequence_num()).unwrap() {
+                    if prepare_msg.get_node_id() == self.get_node_base().node_id {
+                        // broadcast again
+                        broadcastMsg(prepare_msg, "prepare");
+                        break;
+                    }
+                }
+
+            } else {
+                // not same msg for the same num
+                return;
+            }
+
+
+        } else {
+            self.msg_cache.insert(msg.get_view_num(),  msg.get_client_msg().clone());
+            source_msg_option = Some(msg.get_client_msg().clone());
+
+            // do digest by this node
+            let digest = & self.get_node_base().public_key;
+            // new the prepare msg for this node
+
+            // check pass add to prepare cache;
+            let prepare_msg = Bft_Prepare_Message::new(self.view_num, msg.get_sequence_num(), digest.as_str(), self.get_node_base().node_id);
+            if self.prepare_cache.contains_key(& msg.get_sequence_num()) {
+                self.prepare_cache.get(& msg.get_sequence_num()).unwrap().push(prepare_msg);
+            } else {
+                let mut prepare_vec = Vec::new();
+                prepare_vec.push(prepare_msg);
+                self.prepare_cache.insert(msg.get_sequence_num().clone(), prepare_vec);
+            }
+
+            // send the prepare msg
+            broadcastMsg(&prepare_msg, "prepare");
+        }
+    }
+
+    /// receivePrepare msg from other node,
+    /// check msg is valid, if yes ,put to precache list
+    ///
+    pub fn receivePrepare(&mut self, msg:Bft_Prepare_Message) {
+
+        if msg.get_view_num() != self.view_num {
+            return;
+        }
+
+        // check desigt
+        let client_source_msg = msg.get_client_msg();
+        let mut source_msg_option:Option<Bft_Message> = Option::None;
+
+        if self.msg_cache.contains_key(& msg.get_sequence_num()) {
+            // have receive this msg num before, check if the same msg
+            let receive_msg = self.msg_cache.get(& msg.get_sequence_num()).unwrap();
+            if receive_msg.get_id() == msg.get_client_msg().get_id(){
+                // the same
+                source_msg_option = Some(receive_msg.clone());
+            }
+        } else {
+            self.msg_cache.insert(msg.get_view_num(),  msg.get_client_msg().clone());
+            source_msg_option = Some(msg.get_client_msg().clone());
+        }
+
+        // have receive pre prepare msg in this node but not same msg, return error;
+        if  source_msg_option.is_some()
+            && source_msg_option.unwrap().get_id() != client_source_msg.get_id() {
+            return;
+        }
+        // check the desigt
+        let mut node_option:Option<Btf_Node_Simple>  = Option::None;
+        for simple in & self.node_list {
+            if simple.node_id == msg.get_node_id() {
+                node_option = Some(simple.clone());
+            }
+        }
+
+        // not know node, not process its prepare,
+        if node_option.is_none() {
+            return;
+        }
+
+        let simple_node = node_option.unwrap();
+
+        // check design fail
+        if simple_node.public_key != msg.get_public_key() {
+            return;
+        }
+
+        // check pass put the prepare msg to cache
+        if self.prepare_cache.contains_key(& msg.get_sequence_num()) {
+            self.prepare_cache.get(& msg.get_sequence_num()).unwrap().push(msg);
+        } else {
+            let mut prepare_vec = Vec::new();
+            prepare_vec.push(msg);
+            self.prepare_cache.insert(msg.get_sequence_num().clone(), prepare_vec);
+        }
+
+        // check if need to do commit;
+        checkIfCommit(&msg.get_sequence_num());
+
+    }
+
+    fn checkIfCommit(&self, _sequence_num:& u64) {
         if self.prepare_cache.len() <= 0 {
             return;
         }
-        let min_pass_count = self.node_list.le()*2/3 + 1;
-        for prepare_item(num, prepare_msg_list) in self.prepare_cache {
-            if num!= _sequence_num {
-                continue;
+        let min_pass_count = self.node_list.len()*2/3 + 1;
+        let prepare_msg_list_option = self.prepare_cache.get(_sequence_num);
+        if prepare_msg_list_option.is_none() {
+            return
+        }
+
+        let prepare_list = prepare_msg_list_option.unwrap();
+
+        // have enough prepare msg
+        if prepare_list.len()>= min_pass_count {
+            // new commit msg and broadcast the msg _view_num:u32, _sequence_num:u32, _digest:HashValue, _node_id:u32
+            let commit_msg:Bft_Commit_Message = Bft_Commit_Message::new(self.view_num, _sequence_num.clone(), self.get_node_base().public_key.as_str(), self.get_node_base().node_id);
+
+
+
+            // put msg to log file
+            if self.commit_cache.contains_key(_sequence_num) {
+                self.commit_cache.get(_sequence_num).unwrap().push(commit_msg);
+            } else {
+                let mut commit_msg_list = Vec::new();
+                commit_msg_list.push(commit_msg);
             }
 
-            if prepare_msg_list.len()>= min_pass_count {
-                // new commit msg and broadcast the msg
-            }
+            // broadcast the msg to other
+            broadcastMsg(&commit_msg, "commit");
         }
     }
 
+    pub fn receiveCommit(& mut self, msg:Bft_Commit_Message) {
 
-    fn doReplay(&self, _sequence_num:u64) {
-        if self.commit_cache.len() <= 0 {
+        if msg.get_view_num() != self.view_num {
             return;
         }
+
+        if self.msg_cache.contains_key(& msg.get_sequence_num()) {
+            // have receive this msg num before, check if the same msg
+            let receive_msg = self.msg_cache.get(& msg.get_sequence_num()).unwrap();
+            if receive_msg.get_id() == msg.get_client_msg().get_id(){
+                // the same
+                source_msg_option = Some(receive_msg.clone());
+            }
+        } else {
+            self.msg_cache.insert(msg.get_view_num(),  msg.get_client_msg().clone());
+            source_msg_option = Some(msg.get_client_msg().clone());
+        }
+
+        // have receive pre prepare msg in this node but not same msg, return error;
+        if  source_msg_option.is_some()
+            && source_msg_option.unwrap().get_id() != client_source_msg.get_id() {
+            return;
+        }
+
+        let source_msg = source_msg_option.upwrap();
+
+        // check the desigt
+        // check the desigt
+        let mut node_option:Option<Btf_Node_Simple>  = Option::None;
+        for simple in & self.node_list {
+            if simple.node_id == msg.get_node_id() {
+                node_option = Some(simple.clone());
+            }
+        }
+
+        // not know node, not process its prepare,
+        if node_option.is_none() {
+            return;
+        }
+
+        let simple_node = node_option.unwrap();
+
+        // check design fail
+        if simple_node.public_key != msg.get_public_key() {
+            return;
+        }
+
+        // check pass add to prepare cache;
+        if self.commit_cache.contains_key(&msg.get_sequence_num()) {
+            self.commit_cache.get(&msg.get_sequence_num()).unwrap().push(msg);
+        } else {
+            let mut commit_msg_list = Vec::new();
+            commit_msg_list.push(msg);
+            self.commit_cache.insert(msg.get_sequence_num(), commit_msg_list);
+        }
+
+        doReplay(msg.get_sequence_num());
+
+    }
+
+    pub fn doReplay(&self, _sequence_num:u64) {
+        if !self.commit_cache.contains_key(&_sequence_num) {
+            return;
+        }
+
+        // never receive primary node pre prepare, not replay
+        if !self.msg_cache.contains_key(&_sequence_num) {
+            return;
+        }
+        let msg = self.msg_cache.get(&_sequence_num).unwrap();
         /// commit mes count > 2f+1 then pass and view not change commit local;
         ///  commit mes count > f+1 then pass and view have changed commit at this node view;
         let min_pass_count = self.node_list.le()/3 + 1;
-        for prepare_item(num, prepare_msg_list) in self.commit_cache {
-            if num!= _sequence_num {
-                continue;
-            }
-
-            if prepare_msg_list.len()>= min_pass_count {
-                // new replay msg and send to client
-            }
+        let commit_msg_list = self.commit_cache.get(&_sequence_num).unwrap();
+        if commit_msg_list.len()>= min_pass_count {
+            // new replay msg and send to client _view_num:u32, _payload: &str, _node_id:&str, _source_msg:Bft_Message
+            let replay_msg:Bft_Replay = Bft_Replay::new(self.view_num, "succes process", self.get_node_base().node_id.clone(), msg.clone());
+            broadcastMsg(replay_msg, "replay");
         }
     }
 
@@ -102,7 +290,9 @@ impl Btf_Node {
     pub fn start_node(_address:&str, _port:u64) -> Btf_Node{
 
         // send request for primary
+        let node_isntance:Btf_Node;
         if _address.len() > 0 {
+            // the bft network primary not null, is not the first node,send init msg to
             let url = String::from("https://").push_str(_address + ":" + _port);
             let body = reqwest::get(url)?
                 .text()?;
@@ -110,6 +300,8 @@ impl Btf_Node {
             // body is {view_num:num, node_num:xxx, node_list:[{node_num:xxx, address:xxx, port:xxx, public_key:xxx}]};
             let node_value::Value = serde_json::from_str(body)?;
 
+            let view_num = node_value["view_num"].as_u64().unwrap();
+            let node_num = node_value["node_num"].as_u64().unwrap();
             let mut simple_vec:Vec<Btf_Node_Simple> = Vec::new();
             let mut simple_list:Vec<Value> = node_value["node_list"];
             for one_simple in & simple_list {
@@ -121,17 +313,20 @@ impl Btf_Node {
                 };
                 simple_vec.push(simple);
             }
+            let ip = "127.0.0.1";
+            node_isntance = Btf_Node::new(view_num, simple_vec, ip, "8087",node_num);
 
-        }
-        let port = _port;
-        let view_num = 1;
-        let node_list = Vec::new();
-        let ip = "127.0.0.1";
-        let node_id = 1;
-        if _address.len()>0 {
-            // the bft network primary not null, is not the first node,send init msg to
-
+            return node_isntance;
         } else {
+            // 没有其他节点，这个就是第一个节点，第一个视图
+            let port = _port;
+            let view_num = 1;
+            let node_list = Vec::new();
+            let ip = "127.0.0.1";
+            let node_id = 1;
+            node_isntance = Btf_Node::new(view_num, simple_vec, ip, "8087",node_num);
+
+            return node_isntance;
 
         }
 
@@ -146,6 +341,27 @@ impl Btf_Node {
 
         let node = Btf_Node::new(view_num, node_list, ip, port,node_id);
     }
+
+    /// send message to all other node
+    ///
+    fn broadcastMsg<T: Serialize >(&self, data:& T, command:&str) {
+
+        for node in &(self.node_list) {
+            let mut url = String::from("https://").push_str(_address + ":" + _port);
+            url.push("/" + command);
+
+            let mut res = reqwest::Client::new()
+                .post(url.to_str())
+                .json(data)
+                .send()
+                .unwrap();
+            if res.status() !=200 {
+
+            }
+        }
+    }
+
+
 
 
 }
